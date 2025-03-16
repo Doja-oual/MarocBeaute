dc8292fc5bcf0db856a629958de752f14f0e432c
Doja <dojaoualla@gmail.com>
2025-03-16 13:08:06 +0000

    ajoute partie model de Coupon

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable=[
+        'code',
+        'percentage',
+        'expire',
+        'count_use',
+    ];
 }
