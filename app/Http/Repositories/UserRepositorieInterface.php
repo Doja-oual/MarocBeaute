ecdd4ea68cbf0089cd0f8c540636d68b62da5a38
Doja <dojaoualla@gmail.com>
2025-03-16 12:30:10 +0000

    ajoute repositories de user avec interface

---
+++ app/Http/Repositories/UserRepositorieInterface.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Repositories;
+
+interface UserRepositorieInterface {
+
+    public function getById($id);
+    public function getByEmail($email);
+    public function getAll();
+    public function create(array $data);
+
+    public function update($id, array $data);
+
+    public function delete($id);
+}
