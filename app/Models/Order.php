ec93e2ddd144d062caebfee71b24a566238e2067
Doja <dojaoualla@gmail.com>
2025-03-17 06:01:25 +0000

    modifie table orders et ajoute partie model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+   use HasFactory;
+    
+   protected $fillable=[
+    'product_id',
+    'user_id',
+    'qte',
+    'num_order',
+   ];
 }
